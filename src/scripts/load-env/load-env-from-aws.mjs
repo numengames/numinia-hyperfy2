3e4b0d1ea9d6a0875c5d33d54f341f239d535f7b
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2025-01-23 19:52:51 +0100

    fix: Solve an issue with load-env file and attach a script to a pm2 app before running

--- src/scripts/load-env/load-env-from-aws.mjs
+++ src/scripts/load-env/load-env-from-aws.mjs
@@ -1,30 +1,36 @@
 import '@aws-sdk/crc64-nvme-crt';
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
 
-export default async function loadEnvFromAWS() {
+async function loadEnvFromAWS() {
+    const pm2Env = JSON.parse(process.env.pm2_env);
+
+    const AWS_REGION = pm2Env.AWS_REGION;
+    const AWS_SECRET_NAME = pm2Env.AWS_SECRET_NAME;
+
+    if (!AWS_REGION || !AWS_SECRET_NAME) {
+        throw new Error('AWS_REGION and AWS_SECRET_NAME are required');
+    }
+
+    // AWS SDK will automatically look for credentials in:
+    // 1. Environment variables
+    // 2. Shared credentials (~/.aws/credentials)
+    // 3. IAM roles (if running on AWS)
+    const secretsClient = new SecretsManagerClient({ region: AWS_REGION });
+    const secretsCommand = new GetSecretValueCommand({ SecretId: AWS_SECRET_NAME });
+
     try {
-        const AWS_REGION = process.env.AWS_REGION;
-        const AWS_SECRET_NAME = process.env.AWS_SECRET_NAME;
-
-        console.log('AWS_REGION', AWS_REGION);
-        console.log('AWS_SECRET_NAME', AWS_SECRET_NAME);
-
-        // AWS SDK will automatically look for credentials in:
-        // 1. Environment variables
-        // 2. Shared credentials (~/.aws/credentials)
-        // 3. IAM roles (if running on AWS)
-        const secretsClient = new SecretsManagerClient({ region: AWS_REGION });
-
-        const secretsCommand = new GetSecretValueCommand({ SecretId: AWS_SECRET_NAME });
-
         const secretsResponse = await secretsClient.send(secretsCommand);
         const secrets = JSON.parse(secretsResponse.SecretString);
 
         Object.entries(secrets).forEach(([key, value]) => {
             process.env[key] = value;
         });
+
+        console.log('✅ Environment variables loaded from AWS Secrets Manager');
     } catch (error) {
-        console.error('❌ Error loading environment variables from AWS:', error);
-        process.exit(1);
+        console.error('❌ Error loading secrets from AWS:', error.message);
+        throw error;
     }
 }
+
+export default loadEnvFromAWS;
