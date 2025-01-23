1fcd9a86068d5a093c06856f7c317910f3dd100e
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2025-01-23 21:10:35 +0100

    no message

---
+++ src/scripts/index.mjs
@@ -0,0 +1,9 @@
+import loadConfigFromAWS from './load-config-from-aws.mjs'
+
+switch (process.env.CONFIG_PROVIDER) {
+    case 'aws':
+        await loadConfigFromAWS()
+        break
+    default:
+        console.log('No config provider specified')
+}
