afbf9a4383ffcebc97e48481d2466f3bb8ad4d43
DevStarlight <jesushuertaarrabal@gmail.com>
2025-10-01 21:54:10 +0200

    fix: Make COLLECTIONS prop optional in the .env variables

--- src/server/index.js
+++ src/server/index.js
@@ -56,9 +56,6 @@ if (!process.env.ASSETS_BASE_URL) {
 if (process.env.ASSETS === 's3' && !process.env.ASSETS_S3_URI) {
   throw new Error(`[envs] ASSETS_S3_URI must be set when using ASSETS=s3`)
 }
-if (!process.env.COLLECTIONS) {
-  throw new Error(`[envs] COLLECTIONS must be set to 'local' or 's3'`)
-}
 if (process.env.COLLECTIONS === 's3' && !process.env.COLLECTIONS_BASE_URL) {
   throw new Error(`[envs] COLLECTIONS_BASE_URL must be set when using COLLECTIONS=s3`)
 }
