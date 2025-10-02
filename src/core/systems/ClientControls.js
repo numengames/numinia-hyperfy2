4e61de179ee8359badec7979b9e07d3e8e703418
DevStarlight <jesushuertaarrabal@gmail.com>
2025-10-02 17:18:56 +0200

    Revert "feat: xr interact + building"
    This reverts commit b41f8d8c58cbe6decf8fc9a479924e6e6c282b6e.

--- src/core/systems/ClientControls.js
+++ src/core/systems/ClientControls.js
@@ -11,17 +11,6 @@ const MouseRight = 'mouseRight'
 const HandednessLeft = 'left'
 const HandednessRight = 'right'
 
-const XR_TREMOR_SMOOTHING_FACTOR = 0.6
-
-const m1 = new THREE.Matrix4()
-const m2 = new THREE.Matrix4()
-const v1 = new THREE.Vector3()
-const v2 = new THREE.Vector3()
-const q1 = new THREE.Quaternion()
-const q2 = new THREE.Quaternion()
-
-const captured = new Set()
-
 let actionIds = 0
 
 /**
@@ -43,18 +32,12 @@ const controlTypes = {
   pointer: createPointer,
   screen: createScreen,
   camera: createCamera,
-  xrLeftRayPose: createPose,
-  xrLeftGripPose: createPose,
   xrLeftStick: createVector,
   xrLeftTrigger: createButton,
-  xrLeftGrip: createButton,
   xrLeftBtn1: createButton,
   xrLeftBtn2: createButton,
-  xrRightRayPose: createPose,
-  xrRightGripPose: createPose,
   xrRightStick: createVector,
   xrRightTrigger: createButton,
-  xrRightGrip: createButton,
   xrRightBtn1: createButton,
   xrRightBtn2: createButton,
   touchA: createButton,
@@ -91,27 +74,6 @@ export class ClientControls extends System {
     this.world.on('xrSession', this.onXRSession)
   }
 
-  applyXRRig(xrRig) {
-    for (const control of this.controls) {
-      if (control.entries.xrLeftRayPose) {
-        const pose = control.entries.xrLeftRayPose
-        const scale = v1.set(1, 1, 1)
-        m1.compose(pose.lPosition, pose.lQuaternion, scale)
-        m2.compose(xrRig.position, xrRig.quaternion, scale)
-        m1.premultiply(m2)
-        m1.decompose(pose.position, pose.quaternion, scale)
-      }
-      if (control.entries.xrRightRayPose) {
-        const pose = control.entries.xrRightRayPose
-        const scale = v1.set(1, 1, 1)
-        m1.compose(pose.lPosition, pose.lQuaternion, scale)
-        m2.compose(xrRig.position, xrRig.quaternion, scale)
-        m1.premultiply(m2)
-        m1.decompose(pose.position, pose.quaternion, scale)
-      }
-    }
-  }
-
   preFixedUpdate() {
     // mouse wheel delta
     for (const control of this.controls) {
@@ -122,57 +84,18 @@ export class ClientControls extends System {
     }
     // xr
     if (this.xrSession) {
-      const referenceSpace = this.world.graphics.renderer.xr.getReferenceSpace()
-      const frame = this.world.graphics.renderer.xr.getFrame()
-      const player = this.world.entities.player
       this.xrSession.inputSources?.forEach(src => {
         // left
         if (src.gamepad && src.handedness === HandednessLeft) {
-          captured.clear()
           for (const control of this.controls) {
-            if (control.entries.xrLeftRayPose) {
-              if (src.targetRaySpace) {
-                const ray = frame.getPose(src.targetRaySpace, referenceSpace)
-                if (ray) {
-                  const pose = control.entries.xrLeftRayPose
-                  // apply tremor smoothing to local change
-                  if (!pose.lPosition) {
-                    pose.lPosition = new THREE.Vector3().copy(ray.transform.position)
-                    pose.lQuaternion = new THREE.Quaternion().copy(ray.transform.orientation)
-                  }
-                  pose.lPosition.lerp(v1.copy(ray.transform.position), 1 - XR_TREMOR_SMOOTHING_FACTOR)
-                  pose.lQuaternion.slerp(q1.copy(ray.transform.orientation), 1 - XR_TREMOR_SMOOTHING_FACTOR)
-                  // world transform is applied above in this.applyXRRig()
-                }
-              }
-            }
-            if (control.entries.xrLeftGripPose) {
-              if (src.gripSpace) {
-                const grip = frame.getPose(src.gripSpace, referenceSpace)
-                if (grip) {
-                  const pose = control.entries.xrLeftGripPose
-                  // todo: tremor smoothing?
-                  pose.position.copy(grip.transform.position)
-                  pose.quaternion.copy(grip.transform.orientation)
-                  // pose.matrix.fromArray(grip.transform.matrix)
-                }
-              }
-            }
             if (control.entries.xrLeftStick) {
-              if (captured.has('xrLeftStick')) {
-                control.entries.xrLeftStick.value.x = 0
-                control.entries.xrLeftStick.value.z = 0
-              } else {
-                control.entries.xrLeftStick.value.x = src.gamepad.axes[2]
-                control.entries.xrLeftStick.value.z = src.gamepad.axes[3]
-                if (control.entries.xrLeftStick.capture) {
-                  captured.add('xrLeftStick')
-                }
-              }
+              control.entries.xrLeftStick.value.x = src.gamepad.axes[2]
+              control.entries.xrLeftStick.value.z = src.gamepad.axes[3]
+              if (control.entries.xrLeftStick.capture) break
             }
             if (control.entries.xrLeftTrigger) {
               const button = control.entries.xrLeftTrigger
-              const down = src.gamepad.buttons[0].value > 0.9
+              const down = src.gamepad.buttons[0].pressed
               if (down && !button.down) {
                 button.pressed = true
                 button.onPress?.()
@@ -182,21 +105,6 @@ export class ClientControls extends System {
                 button.onRelease?.()
               }
               button.down = down
-              button.value = src.gamepad.buttons[0].value
-            }
-            if (control.entries.xrLeftGrip) {
-              const button = control.entries.xrLeftGrip
-              const down = src.gamepad.buttons[1].pressed
-              if (down && !button.down) {
-                button.pressed = true
-                button.onPress?.()
-              }
-              if (!down && button.down) {
-                button.released = true
-                button.onRelease?.()
-              }
-              button.down = down
-              button.value = src.gamepad.buttons[1].value
             }
             if (control.entries.xrLeftBtn1) {
               const button = control.entries.xrLeftBtn1
@@ -228,51 +136,15 @@ export class ClientControls extends System {
         }
         // right
         if (src.gamepad && src.handedness === HandednessRight) {
-          captured.clear()
           for (const control of this.controls) {
-            if (control.entries.xrRightRayPose) {
-              if (src.targetRaySpace) {
-                const ray = frame.getPose(src.targetRaySpace, referenceSpace)
-                if (ray) {
-                  const pose = control.entries.xrRightRayPose
-                  // apply tremor smoothing to local change
-                  if (!pose.lPosition) {
-                    pose.lPosition = new THREE.Vector3().copy(ray.transform.position)
-                    pose.lQuaternion = new THREE.Quaternion().copy(ray.transform.orientation)
-                  }
-                  pose.lPosition.lerp(v1.copy(ray.transform.position), 1 - XR_TREMOR_SMOOTHING_FACTOR)
-                  pose.lQuaternion.slerp(q1.copy(ray.transform.orientation), 1 - XR_TREMOR_SMOOTHING_FACTOR)
-                  // world transform is applied above in this.applyXRRig()
-                }
-              }
-            }
-            if (control.entries.xrRightGripPose) {
-              if (src.gripSpace) {
-                const grip = frame.getPose(src.gripSpace, referenceSpace)
-                if (grip) {
-                  const pose = control.entries.xrRightGripPose
-                  // todo: tremor smoothing?
-                  pose.position.copy(grip.transform.position)
-                  pose.quaternion.copy(grip.transform.orientation)
-                  // pose.matrix.fromArray(grip.transform.matrix)
-                }
-              }
-            }
             if (control.entries.xrRightStick) {
-              if (captured.has('xrRightStick')) {
-                control.entries.xrRightStick.value.x = 0
-                control.entries.xrRightStick.value.z = 0
-              } else {
-                control.entries.xrRightStick.value.x = src.gamepad.axes[2]
-                control.entries.xrRightStick.value.z = src.gamepad.axes[3]
-                if (control.entries.xrRightStick.capture) {
-                  captured.add('xrRightStick')
-                }
-              }
+              control.entries.xrRightStick.value.x = src.gamepad.axes[2]
+              control.entries.xrRightStick.value.z = src.gamepad.axes[3]
+              if (control.entries.xrRightStick.capture) break
             }
             if (control.entries.xrRightTrigger) {
               const button = control.entries.xrRightTrigger
-              const down = src.gamepad.buttons[0].value > 0.9
+              const down = src.gamepad.buttons[0].pressed
               if (down && !button.down) {
                 button.pressed = true
                 button.onPress?.()
@@ -282,21 +154,6 @@ export class ClientControls extends System {
                 button.onRelease?.()
               }
               button.down = down
-              button.value = src.gamepad.buttons[0].value
-            }
-            if (control.entries.xrRightGrip) {
-              const button = control.entries.xrRightGrip
-              const down = src.gamepad.buttons[1].pressed
-              if (down && !button.down) {
-                button.pressed = true
-                button.onPress?.()
-              }
-              if (!down && button.down) {
-                button.released = true
-                button.onRelease?.()
-              }
-              button.down = down
-              button.value = src.gamepad.buttons[1].value
             }
             if (control.entries.xrRightBtn1) {
               const button = control.entries.xrRightBtn1
@@ -818,7 +675,6 @@ function createButton(controls, control, prop) {
   const released = false
   return {
     $button: true,
-    value: 0, // eg xr triggers
     down,
     pressed,
     released,
@@ -833,16 +689,6 @@ function createVector(controls, control, prop) {
     $vector: true,
     value: new THREE.Vector3(),
     capture: false,
-  }
-}
-
-function createPose(controls, control, prop) {
-  return {
-    $pose: true,
-    position: new THREE.Vector3(),
-    quaternion: new THREE.Quaternion(),
-    // matrix: new THREE.Matrix4(),
-    // capture: false,
   }
 }
 
